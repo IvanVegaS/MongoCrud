8b18bf4899cf037224f6ca202d726071419ad980
IvanAlejandroVegaFlo <IvanAlejandroVegaFlo@192.168.1.71>
2020-02-20 14:06:36 -0600

    Updated CORS mapping to add allow methods.

--- src/main/java/com/ivcoding/mongobasiccrud/config/WebConfig.java
+++ src/main/java/com/ivcoding/mongobasiccrud/config/WebConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
 	}
 
 }
