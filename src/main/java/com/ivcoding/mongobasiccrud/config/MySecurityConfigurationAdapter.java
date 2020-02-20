0c29cedcd0a6ed88004e2d0428ab30ab38585486
IvanAlejandroVegaFlo <IvanAlejandroVegaFlo@192.168.1.71>
2020-02-19 23:35:40 -0600

    Updated listening port to match AWS.

--- src/main/java/com/ivcoding/mongobasiccrud/config/MySecurityConfigurationAdapter.java
+++ src/main/java/com/ivcoding/mongobasiccrud/config/MySecurityConfigurationAdapter.java
@@ -2,6 +2,7 @@ package com.ivcoding.mongobasiccrud.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,4 +13,8 @@ public class MySecurityConfigurationAdapter extends WebSecurityConfigurerAdapter
 		web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
 	}
 
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().permitAll();
+	}
 }
